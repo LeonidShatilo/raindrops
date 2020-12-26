464c90048ea5bd0663ac69454d8343d11a703f6d
Leonid Shatilo <shatiloleonid@gmail.com>
2020-12-26 23:24:56 +0300

    feat: implement navigation to other pages

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,7 @@
-const fullscreen = document.getElementById('fullscreen'),
-      container = document.querySelector('.container');
+const container = document.querySelector('.container');
+const fullscreen = document.getElementById('fullscreen');
+const play = document.getElementById('play');
+const tutorial = document.getElementById('tutorial');
 
 fullscreen.addEventListener('click', () => {
   if (document.fullscreenElement) {
@@ -7,4 +9,12 @@ fullscreen.addEventListener('click', () => {
   } else {
     container.requestFullscreen();
   }
+});
+
+play.addEventListener('click', () => {
+  document.location.href = './src/play.html';
+});
+
+tutorial.addEventListener('click', () => {
+  document.location.href = './src/tutorial.html';
 });
