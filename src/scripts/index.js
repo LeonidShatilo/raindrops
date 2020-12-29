2087b312966b1ee6869d60c10202692e195eafb7
Leonid Shatilo <shatiloleonid@gmail.com>
2020-12-29 19:41:20 +0300

    fix: delete fullscreen button

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,15 +1,5 @@
-const container = document.querySelector('.container');
-const fullscreen = document.getElementById('fullscreen');
 const play = document.getElementById('play');
 const tutorial = document.getElementById('tutorial');
-
-fullscreen.addEventListener('click', () => {
-  if (document.fullscreenElement) {
-    document.exitFullscreen();
-  } else {
-    container.requestFullscreen();
-  }
-});
 
 play.addEventListener('click', () => {
   document.location.href = './src/play.html';
